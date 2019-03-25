e20b2d2fa523c8423fb4b792773a236e3ebf96fc
Petr Bojda <petr.bojda@email.cz>
2019-03-25 17:05:37 +0100

    synchro

--- rtl/src/hdl/jtag_engine.vhd
+++ rtl/src/hdl/jtag_engine.vhd
@@ -97,160 +97,46 @@ begin  -- architecture behavi
   -----------------------------------------------------------------------------
 
   
-  spi_tx : process (clk_in, rst_n) is
+  spi_tx : process (clk_in, rst_n, enable_red, tck_pulse, bit_count) is
   begin  -- process spi_inoutregister
     if rst_n = '0' then
       main_fsm      <= IDLE;
-      spi_addr_reg  <= (others => '0');
-      spi_rdata_reg <= (others => '0');
-      spi_tdata_reg <= (others => '0');
-
+      tck_en <= '0';
+      done_i <= '0';
+          
     elsif clk_in'event and clk_in = '1' then  -- rising clock edge
 
       case main_fsm is
         when IDLE =>
           ---------------------------------------------------------------------
-          main_fsm              <= CFG_IN when cfg_valid else IDLE;
-          spi_data_count        <= (others => '0');
-          spi_addr_count        <= (others => '0');
-          spi_trans_count       <= (others => '0');
-          spi_rw_mode_buff      <= '0';
-          spi_ss_select_buff    <= "000";
-          num_data_bytes_buff   <= "00000";
-          num_transactions_buff <= "00";
-
-          dev_ready      <= '1';
-          rdata_ready    <= '0';
-          cfg_recved     <= '0';
-          tdata_recved   <= '0';
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '0';
-
-          spi_mosi <= '0';
-
-        when CFG_IN =>
+          main_fsm   <= TCKL when enable_red else IDLE;
+          tck_en <= '0';
+          done_i <= '0';
+          
+        when TCKL =>
           ---------------------------------------------------------------------
-          main_fsm              <= DATA_IN when tdata_valid else CFG_IN;
-          spi_data_count        <= num_data_bytes;
-          spi_trans_count       <= num_transactions;
-          spi_rw_mode_buff      <= spi_rw_mode;
-          spi_ss_select_buff    <= spi_ss_select;
-          num_data_bytes_buff   <= num_data_bytes;
-          num_transactions_buff <= num_transactions;
-
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= '1';
-          tdata_recved   <= '0';
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '0';
-
-        when DATA_IN =>
+          main_fsm   <= TCKH when tck_pulse else TCKL;
+          tck_en <= '0';
+          done_i <= '0';
+          
+        when TCKH =>
           ---------------------------------------------------------------------
-          main_fsm      <= DATA_PREP;
-          r_tdata_array <= spi_tdata_array;
-          r_addr_array  <= spi_addr_array;
-
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= '1';
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '1';
-
-        when N_O_TRANS_DECREMENTING =>
-          ---------------------------------------------------------------------
-          main_fsm        <= DATA_PREP;
-          spi_trans_count <= spi_trans_count - '1';
-
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= tdata_recved;
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '1';
-
-        when DATA_PREP =>
-          ---------------------------------------------------------------------
-          main_fsm      <= SPI_ADDRESS_OUT;
-          spi_addr_reg  <= spi_addr_array(to_integer(unsigned(spi_trans_count)));
-          spi_tdata_reg <= spi_tdata_array(to_integer(unsigned(spi_trans_count)));
-          spi_mosi      <= '0';
-
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= tdata_recved;
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '1';
-
-                                        -- TODO: Each consecutive
-                                        -- transaction requires
-                                        -- signal CS to go High and
-                                        -- Low again.
-
-        when SPI_ADDRESS_OUT =>
-          ---------------------------------------------------------------------
-          -- TODO: Clock out the address
-          if spi_addr_count = (spi_addr_count'range => '0') then
-            main_fsm <= SPI_TRANSACTION;
-          else
-            main_fsm <= SPI_ADDRESS_OUT;
-          end if;
-          spi_mosi       <= spi_addr_reg(spi_addr_reg'left);
-          spi_addr_reg   <= spi_addr_reg(spi_addr_reg'left - 1 downto 0) & '0';
-          spi_addr_count <= spi_addr_count - '1';
-
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= tdata_recved;
-          tdata_sent     <= '0';
-          spi_ss_encoder <= '1';
-
-        when SPI_TRANSACTION =>
-          ---------------------------------------------------------------------
-          if spi_data_count = (spi_data_count'range => '0') then
-            if spi_trans_count = (spi_trans_count'range => '0') then
-              main_fsm <= DATA_TO_MASTER;
-            else
-              main_fsm <= N_O_TRANS_DECREMENTING;
-            end if;
-          else
-            main_fsm <= SPI_TRANSACTION;
-          end if;
-
-          spi_tdata_reg  <= spi_tdata_reg(spi_tdata_reg'left - 1 downto 0) & '0';
-          spi_mosi       <= spi_tdata_reg(to_integer(unsigned(num_data_bytes)));
-          spi_data_count <= spi_data_count - '1';
-          dev_ready      <= '0';
-          rdata_ready    <= '0';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= tdata_recved;
-          tdata_sent     <= '1';
-          spi_ss_encoder <= '1';
-
-        when DATA_TO_MASTER =>
-          ---------------------------------------------------------------------
-          spi_mosi <= '0';
-
-          if spi_rw_mode_buff then
-            main_fsm <= IDLE;
-          else
-            if rdata_read then
+          if tck_pulse then
+            if bit_count = 0 then
               main_fsm <= IDLE;
+              tck_en <= '0';
+              done_i <= '0';
             else
-              main_fsm <= DATA_TO_MASTER;
-            end if;
-          end if;
-
-          dev_ready      <= '0';
-          rdata_ready    <= '1';
-          cfg_recved     <= cfg_recved;
-          tdata_recved   <= tdata_recved;
-          tdata_sent     <= '1';
-          spi_ss_encoder <= '0';
-
+              main_fsm <= TCKL;
+              tck_en <= '0';
+              done_i <= '0';
+            end;
+          else
+            main_fsm <= TCKH;
+            tck_en <= '0';
+            done_i <= '0';
+          end;
+          
       end case;
     end if;
   end process spi_tx;
